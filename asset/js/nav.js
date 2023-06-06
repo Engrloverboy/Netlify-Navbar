50d883c4b2919e186314d9bf791836ab8dcbd571
sodiqmakinde <msodq2018@gmail.com>
2023-06-06 12:44:38 +0100

    I rectify the navbar

--- asset/js/nav.js
+++ asset/js/nav.js
@@ -2,37 +2,13 @@ const closeBtn = document.querySelector(".close");
 const navBar = document.querySelector(".top_nav");
 
 const navToogler = document.querySelector(".hambuger");
-const navWrapper = document.querySelector(".ttt");
-
-// function removenavbar() {
-//   navBar.style.display = "none";
-// }
-// navBar.addEventListener("click", removenavbar);
-
-// OR
-
-// const removenavbar = () => {
-//   navBar.style.display = "none";
-//   console.log("Good Morning");
-// };
-// navBar.addEventListener("click", removenavbar);
-
-// OR
+const navWrapper = document.querySelector(".multi_step");
 
 const removenavbar = () => {
   navBar.classList.add("navbarRemover");
 };
 navBar.addEventListener("click", removenavbar);
 
-// const hambugerbtn = document.querySelector(".hambuger");
-// const opennav = document.querySelector(".ff_nav");
-
-// const menuHambuger = () => {
-
-// };
-// opennav.addEventListener("click", menuHambuger);
-
 navToogler.addEventListener("click", () => {
-  console.log("Hello World");
   navWrapper.classList.toggle("active");
 });
